586b106708c53b3413dd73f1407b6411c467bb36
Richard Blažek <blazek.risa@gmail.com>
2021-04-15 19:24:41 +0200

    Vigenere bug fixes

--- SIMSA/Models/VigenereText.cs
+++ SIMSA/Models/VigenereText.cs
@@ -6,10 +6,12 @@ namespace SIMSA.Models
 	{
 		public string Text { get; }
 		public string Key { get; }
-		public VigenereText(string text = "", string key = "")
+		public bool Minus { get; }
+		public VigenereText(string text = "", string key = "", bool minus = false)
 		{
 			Text = text;
 			Key = key;
+			Minus = minus;
 		}
 		
 		public string DecipheredIn(IAlphabet alphabet)
@@ -22,19 +24,12 @@ namespace SIMSA.Models
 			foreach (var (textPart, keyPart) in Text.DivideToUnicodeChars().Zip(Key.DivideToUnicodeChars().Forever()))
 			{
 				int textCode = alphabet.IndexOf(textPart), keyCode = alphabet.IndexOf(keyPart);
-				_ = result.Append(textCode == -1 || keyCode == -1 ? textPart : alphabet[textCode + keyCode - alphabet.ZeroIndex]);
+				_ = result.Append(textCode == -1 || keyCode == -1 ? textPart : alphabet[textCode + keyCode * (Minus ? -1 : 1)]);
 			}
 			return result.ToString();
 		}
-		public VigenereText InvertKey(IAlphabet alphabet)
-		{
-			var result = new StringBuilder(Key.Length);
-			foreach (string part in Key.DivideToUnicodeChars())
-			{
-				int code = alphabet.IndexOf(part);
-				_ = result.Append(code == -1 ? part : alphabet[^(code - alphabet.ZeroIndex * 2)]);
-			}
-			return new VigenereText(Text, result.ToString());
-		}
+		public VigenereText InvertSign() => new VigenereText(Text, Key, !Minus);
+		public VigenereText WithText(string text) => new VigenereText(text, Key, Minus);
+		public VigenereText WithKey(string key) => new VigenereText(Text, key, Minus);
 	}
 }
--- SIMSA/Pages/Vigenere.xaml.cs
+++ SIMSA/Pages/Vigenere.xaml.cs
@@ -10,21 +10,17 @@ namespace SIMSA.Pages
 		public Config Config { get; set; }
 		VigenereText text;
 		IAlphabet alphabet;
-		void SetText(VigenereText newText, bool changeEntries)
+		void SetText(VigenereText newText)
 		{
 			text = newText;
+			sign.Text = text.Minus ? "\u2212" : "+";
 			output.Text = text.DecipheredIn(alphabet);
-			if (changeEntries)
-			{
-				input.Text = text.Text;
-				key.Text = text.Key;
-			}
 		}
 		void SetAlphabet(IAlphabet newAlphabet)
 		{
 			alphabet = newAlphabet;
 			alphabetBtn.Text = newAlphabet.Name;
-			SetText(text, false);
+			output.Text = text.DecipheredIn(alphabet);
 		}
 		public Vigenere(Config config, VigenereText initText)
 		{
@@ -36,9 +32,9 @@ namespace SIMSA.Pages
 			key.Keyboard = Keyboard.Create(KeyboardFlags.CapitalizeCharacter);
 
 			alphabetBtn.Clicked += async (o, a) => await Navigation.PushAsync(new SelectAlphabet(Config.Alphabets, SetAlphabet), false);
-			input.TextChanged += (o, a) => SetText(new VigenereText(a.NewTextValue.ToUpper(), text.Key), false);
-			key.TextChanged += (o, a) => SetText(new VigenereText(text.Text, a.NewTextValue.ToUpper()), false);
-			invert.Clicked += (o, a) => SetText(text.InvertKey(alphabet), true);
+			input.TextChanged += (o, a) => SetText(text.WithText(a.NewTextValue.ToUpper()));
+			key.TextChanged += (o, a) => SetText(text.WithKey(a.NewTextValue.ToUpper()));
+			sign.Clicked += (o, a) => SetText(text.InvertSign());
 
 			SetAlphabet(config.DefaultAlphabet);
 		}
